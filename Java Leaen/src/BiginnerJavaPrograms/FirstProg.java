47c324e0c86cc83d88a896c702f84b8830640bac
ypc37 <ypc37@LAPTOP-J8VFD83E>
2021-05-09 13:29:57 +0530

    Created First Java Program

---
+++ Java Leaen/src/BiginnerJavaPrograms/FirstProg.java
@@ -0,0 +1,11 @@
+package BiginnerJavaPrograms;
+
+public class FirstProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is my FIrst Java Program");
+	}
+
+}
